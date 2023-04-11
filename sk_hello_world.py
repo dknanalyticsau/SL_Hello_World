085c335770b7f33940b20a7eafe3f019cdab2983
dknanalyticsau <68187665+dknanalyticsau@users.noreply.github.com>
2023-04-12 09:49:02 +1000

    Update sk_hello_world.py

--- sk_hello_world.py
+++ sk_hello_world.py
@@ -2,11 +2,6 @@
 import streamlit
 import pandas as pd
 
-products_list = [['laptop',1300],['printer',150],['tablet',300],['desk',450],['chair',200]]
-
-df = pd.DataFrame (products_list, columns = ['product_name', 'price'])
-print (df)
-
 streamlit.title('hello world')
 
 streamlit.header('header')
