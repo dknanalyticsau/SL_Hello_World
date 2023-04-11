1dca3c3f9e59c7daf283a46063df5276317ce217
dknanalyticsau <68187665+dknanalyticsau@users.noreply.github.com>
2023-04-12 09:46:26 +1000

    Update sk_hello_world.py

--- sk_hello_world.py
+++ sk_hello_world.py
@@ -8,6 +8,8 @@ streamlit.header('header')
 streamlit.text('text')
 streamlit.text('This is amn update')
 
+uploaded_file_ = st.file_uploader('Upload File Here')
+
 products_list = [['laptop',1300],['printer',150],['tablet',300],['desk',450],['chair',200]]
 
 df = pd.DataFrame (products_list, columns = ['product_name', 'price'])
