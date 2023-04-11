baa8946eee4b7d0913978082ad79a883bc77cb15
dknanalyticsau <68187665+dknanalyticsau@users.noreply.github.com>
2023-04-11 19:41:39 +1000

    Update sk_hello_world.py

--- sk_hello_world.py
+++ sk_hello_world.py
@@ -1,7 +1,13 @@
 
 import streamlit
+import pandas as pd
 
 streamlit.title('hello world')
 
 streamlit.header('header')
 streamlit.text('text')
+
+products_list = [['laptop',1300],['printer',150],['tablet',300],['desk',450],['chair',200]]
+
+df = pd.DataFrame (products_list, columns = ['product_name', 'price'])
+print (df)
